558233533aecf11f6bd62da02338af234e5b92e9
Rahman Tech <rahmantech22@gmail.com>
2025-08-08 10:52:16 +0500

    RAHMAN-MD

---
+++ plugins/tool-trt.js
@@ -0,0 +1,46 @@
+const axios = require('axios');
+const config = require('../config')
+const {cmd , commands} = require('../command')
+const googleTTS = require('google-tts-api')
+
+cmd({
+    pattern: "trt",
+    alias: ["translate"],
+    desc: "🌍 Translate text between languages",
+    react: "⚡",
+    category: "other",
+    filename: __filename
+},
+async (conn, mek, m, { from, q, reply }) => {
+    try {
+        const args = q.split(' ');
+        if (args.length < 2) return reply("❗ Please provide a language code and text. Usage: .translate [language code] [text]");
+
+        const targetLang = args[0];
+        const textToTranslate = args.slice(1).join(' ');
+
+        const url = `https://api.mymemory.translated.net/get?q=${encodeURIComponent(textToTranslate)}&langpair=en|${targetLang}`;
+
+        const response = await axios.get(url);
+        const translation = response.data.responseData.translatedText;
+
+        const translationMessage = `‎╭╼╼╼╼╼╼╼╼╼╼╼╼╼╼╼╼╼╼╼╼╮
+‎      *_ʀᴀʜᴍᴀɴ ᴍᴅ ᴛʀᴀɴsʟᴀᴛɪᴏɴ_*
+‎
+‎ Oʀɪɢɪɴᴀʟ: ${textToTranslate}
+‎
+‎Tʀᴀɴsʟᴀᴛᴇᴅ: ${translation}
+‎
+‎Lᴀɴɢᴜᴀɢᴇ: ${targetLang.toUpperCase()}
+‎
+‎╰╼╼╼╼╼╼╼╼╼╼╼╼╼╼╼╼╼╼╼╼╯
+‎╭──────────────────━┈⍟
+‎┋ *_ᴘᴏᴡᴇʀᴇᴅ ʙʏ ʀᴀʜᴍᴀɴ-ᴛᴇᴄʜ_* 
+‎╰──────────────────━┈⍟`;
+
+        return reply(translationMessage);
+    } catch (e) {
+        console.log(e);
+        return reply("⚠️ An error occurred data while translating the your text. Please try again later🤕");
+    }
+});
